8d5a492426e7830794b1dc0648b5751adbd3f06d
NgomMoustapha <96490893+NgomMoustapha@users.noreply.github.com>
2022-07-17 16:17:20 +0200

    Add files via upload

---
+++ tabs/intro.py
@@ -0,0 +1,38 @@
+from grpc import StreamUnaryClientInterceptor
+import streamlit as st
+
+
+title = "Introduction : Mini-Projet SQL"
+sidebar_name = "Introduction"
+
+
+def run():
+
+    st.markdown("---")
+
+    st.title(title)
+
+    st.markdown("---")
+
+    st.subheader('Language SQL')
+
+    st.markdown(
+        """
+        **Ce streamlit a pour but de mettre en avant les bases que j'ai acquise au cours de ma certification SQL. 
+        La page suivante montre une illustration des requêtes ,qui ont déja ete faites en local,et transformées en Data Frame.
+        J'ai abordé des concepts fondamentaux de requêtages. 
+        J'ai utilisé SQL Alchemy qui est une librairie Python très utilisée pour se connecter à un système de gestion de bases de données relationnelles. Il permet d'interpréter du code SQL.**
+        Le notebook Python est disponible sur Github
+
+        """
+
+    )
+    st.header("La base de données")
+
+    st.markdown(
+        """
+        J'utilise la base de données imdb disponible en Open Source: https://www.imdb.com/interfaces/. 
+
+    
+        """
+    )
---
+++ tabs/second_tab.py
@@ -0,0 +1,107 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+
+title = "Requêtes et Dataviz"
+sidebar_name = "Requêtes et Dataviz"
+
+def run():
+
+    st.title(title)
+
+    
+
+    st.markdown(
+        """
+        ***Analyse des tables et de leurs attributs.***
+        """
+    )
+
+    choice = st.selectbox(label="Choisissez une table pour afficher ses attributs",options =['akas','crew','episodes','people','ratings','titles'])
+
+    if st.button("Afficher les attributs de la table"):
+        table_affiche=pd.read_csv("/home/moustapha/Desktop/ProjetPerso/projetsql/mini_sql_project/data/"+choice+".csv")
+        st.dataframe(table_affiche)
+
+    st.markdown(
+        """
+        ***-------------------------------------------------------------------------------------------------------------------------------------------***
+        """
+    )
+
+    st.markdown(
+        """
+        **Les 50 titres les mieux notés parmis ceux qui ont au mois 5000 votants**
+
+        """
+    )
+    meilleurs_titres=pd.read_csv('/home/moustapha/Desktop/ProjetPerso/projetsql/mini_sql_project/data/50_meilleurs_titres.csv')
+    
+
+
+    st.dataframe(meilleurs_titres)
+
+    st.markdown(
+        """
+        ***-------------------------------------------------------------------------------------------------------------------------------------------***
+        """
+    )
+    
+    st.markdown("""
+     **Liste des films dans lequel ces 3 personnes ont participé.**
+     """
+    )
+
+
+    choice2 = st.selectbox(label="Choisissez une table pour afficher ses attributs",options =['Denzel_Washington','Quentin_Tarantino','George_Clooney'])
+
+    if st.button("Afficher les films"):
+        table_affiche2=pd.read_csv("/home/moustapha/Desktop/ProjetPerso/projetsql/mini_sql_project/data/"+choice2+".csv")
+        st.dataframe(table_affiche2)
+    
+    st.markdown(
+        """
+        ***-------------------------------------------------------------------------------------------------------------------------------------------***
+        """
+    )
+    st.markdown(
+        """
+        **Nombre de titres par type**
+
+        """
+    )
+    
+    titres_par_type=pd.read_csv('/home/moustapha/Desktop/ProjetPerso/projetsql/mini_sql_project/data/nombre_de_titre_par_type.csv',index_col='type')
+    
+
+
+    
+
+    st.bar_chart(titres_par_type['nombre de titres'])
+    st.markdown(
+        """
+        On a logiquement plus de titres pour les séries, chaque épisode d'une serie avec son titre.
+        """
+    )
+
+    st.markdown(
+        """
+        ***-------------------------------------------------------------------------------------------------------------------------------------------***
+        """
+    )
+    
+    st.markdown(
+        """
+        **Les dix genres ayant obtenus une meilleure note moyenne**
+        """
+    )
+    notes_par_genre=pd.read_csv('/home/moustapha/Desktop/ProjetPerso/projetsql/mini_sql_project/data/note_moyenne_par_genre.csv')
+    st.dataframe(notes_par_genre)
+
+
+    
+
