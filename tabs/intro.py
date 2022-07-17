f3cd1a267ad32f1185506b151db4ea2230bf1a9d
NgomMoustapha <96490893+NgomMoustapha@users.noreply.github.com>
2022-07-17 16:49:49 +0200

    Update intro.py

--- tabs/intro.py
+++ tabs/intro.py
@@ -18,10 +18,10 @@ def run():
     st.markdown(
         """
         **Ce streamlit a pour but de mettre en avant les bases que j'ai acquise au cours de ma certification SQL. 
-        La page suivante montre une illustration des requêtes ,qui ont déja ete faites en local,et transformées en Data Frame.
+        La page suivante montre une illustration des requêtes ,qui ont déja été faites en local, transformées en Data Frame, puis transportées sur Github.
         J'ai abordé des concepts fondamentaux de requêtages. 
-        J'ai utilisé SQL Alchemy qui est une librairie Python très utilisée pour se connecter à un système de gestion de bases de données relationnelles. Il permet d'interpréter du code SQL.**
-        Le notebook Python est disponible sur Github
+        J'ai utilisé SQL Alchemy qui est une librairie Python très utilisée pour se connecter à un système de gestion de bases de données relationnelles. Il permet d'interpréter du code SQL.
+        Le notebook Python est disponible sur Github.**
 
         """
 
