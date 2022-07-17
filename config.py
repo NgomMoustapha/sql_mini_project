2182c2eb5e8aaf827a029a3e60be321bb1c64f40
NgomMoustapha <96490893+NgomMoustapha@users.noreply.github.com>
2022-07-17 16:16:17 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+"""
+
+Config file for Streamlit App
+
+"""
+
+from member import Member
+
+
+TITLE = "SQL MINI PROJECT"
+
+AUTEUR = [
+    Member(
+        name="Moustapha Ngom",
+        linkedin_url="https://www.linkedin.com/in/moustapha-ngom/",
+        github_url="https://github.com/NgomMoustapha",
+    )
+
+]
+
+
